5ba3bde897367fb3fa30aa9f3b030bf323fd934a
achyuth reddy <ncsachyuth@gmail.com>
2021-01-01 02:41:51 +0530

    created the useTasks hook

---
+++ src/hooks/index.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import { firebase } from '../firebase'
+import moment from 'moment'
+import { collatedTasksExist } from '../helpers'
+
+export const useTasks = selectedProject => {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    let unsubscribe = firebase
+      .firestore()
+      .collection('tasks')
+      .where('userId', '==', 'wlesFIORl2EcH358TTAA')
+
+    unsubscribe =
+      selectedProject && !collatedTasksExist(selectedProject)
+        ? (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
+        : selectedProject === 'TODAY'
+        ? (unsubscribe = unsubscribe.where(
+            'date',
+            '==',
+            moment().format('DD/MM/YYYY')
+          ))
+        : selectedProject === 'INBOX' || selectedProject === 0
+        ? (unsubscribe = unsubscribe.where('date', '==', ''))
+        : unsubscribe
+  }, [selectedProject])
+}
