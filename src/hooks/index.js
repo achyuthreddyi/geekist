f25ae5821edf033add91b481ead6c96b0e3286d9
achyuth reddy <ncsachyuth@gmail.com>
2021-01-01 03:14:58 +0530

    created the useProjects hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -48,3 +48,26 @@ export const useTasks = selectedProject => {
   }, [selectedProject])
   return { tasks, archivedTasks }
 }
+
+export const useProjects = () => {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    firebase.firestore
+      .collection('projects')
+      .where('uerId', '==', 'wlesFIORl2EcH358TTAA')
+      .orderBy('projectId')
+      .get()
+      .then(snapshot => {
+        const allProjects = snapshot.docs.map(project => ({
+          ...project.data(),
+          docId: project.id
+        }))
+
+        if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+          setProjects(allProjects)
+        }
+      })
+  }, [projects])
+  return { projects, setProjects }
+}
