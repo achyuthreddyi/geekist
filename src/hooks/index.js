83ac7e03483d9a2c963c825301f0edc741f13872
achyuth reddy <ncsachyuth@gmail.com>
2021-01-01 02:59:40 +0530

    updated the useTasks hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -5,6 +5,7 @@ import { collatedTasksExist } from '../helpers'
 
 export const useTasks = selectedProject => {
   const [tasks, setTasks] = useState([])
+  const [archivedTasks, setArchivedTasks] = useState([])
 
   useEffect(() => {
     let unsubscribe = firebase
@@ -24,5 +25,26 @@ export const useTasks = selectedProject => {
         : selectedProject === 'INBOX' || selectedProject === 0
         ? (unsubscribe = unsubscribe.where('date', '==', ''))
         : unsubscribe
+
+    unsubscribe = unsubscribe.onSnapshot(snapshot => {
+      const newTasks = snapshot.docs.map(task => ({
+        id: task.id,
+        ...task.data()
+      }))
+
+      setTasks(
+        selectedProject === 'NEXT_7'
+          ? newTasks.filter(
+              task =>
+                moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+                task.archived !== true
+            )
+          : newTasks.filter(task => task.archived !== true)
+      )
+
+      setArchivedTasks(newTasks.filter(task => task.archived !== false))
+    })
+    return () => unsubscribe()
   }, [selectedProject])
+  return { tasks, archivedTasks }
 }
