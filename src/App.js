997a43ffd0b7f5bc4e64681204aa4e8da32c1753
achyuth reddy <ncsachyuth@gmail.com>
2021-01-01 00:27:00 +0530

    created the basic header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Header } from './components/layout/Header'
 
 export const App = () => (
   <div>
+    <Header />
+
     <h1>happy new year 12fgf</h1>
   </div>
 )
