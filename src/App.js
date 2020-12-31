93205a9d4b93d0401fce9263342b9fa83be07d3a
achyuth reddy <ncsachyuth@gmail.com>
2021-01-01 00:10:43 +0530

    created all the folders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-function App () {
-  return <div className='App'></div>
-}
+import React from 'react'
 
-export default App
+export const App = () => (
+  <div>
+    <h1>happy new year 12fgf</h1>
+  </div>
+)
